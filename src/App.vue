67dd07d1a6aab34d333bee04d0f5f5e5ad0e5382
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-15 15:34:58 +0100

    ADD > Initial setup + ProjectIndex component

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+import ProjectIndex from './pages/projects/Index.vue';
+
+export default {
+  components: {
+    ProjectIndex
+  },
+
+  data() {
+    return {
+      page: 'App.vue'
+    }
+  }
+}
+</script>
+
+<template>
+  <ProjectIndex/>
+</template>
+
+<style lang="scss">
+@use './style/global.scss';
+</style>
---
+++ src/pages/projects/Index.vue
@@ -0,0 +1,21 @@
+<script>
+</script>
+
+<template>
+
+  <section class="projects section">
+    <div class="container">
+      <h1>Tutti i miei progetti</h1>
+      <div class="projects__inner">
+
+        <!-- card dei progetti -->
+
+      </div>
+    </div>
+  </section>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
