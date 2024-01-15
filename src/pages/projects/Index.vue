199474bdab4bb177e9f43d5d5b6ef8e5a6f22a4c
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-15 16:27:53 +0100

    ADD > Axios call inside index project page and import projectCard component

--- src/pages/projects/Index.vue
+++ src/pages/projects/Index.vue
@@ -1,14 +1,44 @@
 <script>
+import axios from 'axios';
+import ProjectCard from '../../components/ProjectCard.vue';
+
+export default {
+  components: {
+    ProjectCard
+  },
+
+  data() {
+    return {
+      BASE_URL: 'http://127.0.0.1:8000/api',
+      projects: []
+    }
+  },
+
+  methods: {
+    fetchProjects() {
+      axios
+        .get(`${this.BASE_URL}/projects`)
+        .then((res)=> {
+          this.projects = res.data.results;
+        })
+    }
+  },
+
+  created() {
+    this.fetchProjects();
+  }
+}
 </script>
 
 <template>
 
   <section class="projects section">
     <div class="container">
-      <h1>Tutti i miei progetti</h1>
+      <h1 class="main-title">Tutti i miei progetti</h1>
       <div class="projects__inner">
 
         <!-- card dei progetti -->
+        <ProjectCard :project="project" v-for="project in projects" :key="project.id" />
 
       </div>
     </div>
@@ -17,5 +47,19 @@
 </template>
 
 <style lang="scss" scoped>
+
+  .projects {
+
+    .main-title {
+      font-size: 1.6rem;
+      margin-bottom: 50px;
+    }
+
+    &__inner {
+      display: grid;
+      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      gap: 40px;
+    }
+  }
 
 </style>
