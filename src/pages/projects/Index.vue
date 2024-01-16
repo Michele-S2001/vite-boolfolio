89d0c73f2825bdb77ccb5d5b9c930d0a546f95be
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-16 10:41:54 +0100

    CHORE > Grid changes

--- src/pages/projects/Index.vue
+++ src/pages/projects/Index.vue
@@ -93,7 +93,7 @@ export default {
 
     &__inner {
       display: grid;
-      grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
       gap: 40px;
     }
   }
