51821f5a1b8080068cc13857a4203ded5295ff51
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-16 15:43:20 +0100

    ADD > Draft of Home page

---
+++ src/pages/AppHome.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="hero">
+    <div class="container">
+      <div class="hero__inner">
+        <h1>Welcome to boolfolio!</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+
+  .hero {
+    padding: 150px 0;
+
+    &__inner {
+      text-align: center;
+    }
+  }
+
+</style>
