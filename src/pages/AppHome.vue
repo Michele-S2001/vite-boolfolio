fa9a5d5b2158db260e900f7eb77bfab89a4023d6
MicheleSerafini <serafini.michele01@gmail.com>
2024-01-17 12:29:00 +0100

    ADD > AppHome styling with desc and a profile img

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -2,7 +2,14 @@
   <section class="hero">
     <div class="container">
       <div class="hero__inner">
-        <h1>Welcome to boolfolio!</h1>
+        <div class="introduction">
+          <h1 class="greeting">Hi! I'm Michele Serafini</h1>
+          <p class="job">Full-stack web developer</p>
+          <p class="bio">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis beatae consectetur accusamus ut repellat voluptatibus, illum assumenda? Blanditiis, magnam distinctio? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas qui explicabo voluptatem nisi nesciunt incidunt eos quasi! Maxime facere voluptate illo laborum! Veritatis sapiente quae modi doloremque commodi harum voluptatem.</p>
+        </div>
+        <div class="image">
+          <img src="/img/Profilo-removebg.png" alt="Profile image">
+        </div>
       </div>
     </div>
   </section>
@@ -11,10 +18,40 @@
 <style lang="scss" scoped>
 
   .hero {
-    padding: 150px 0;
+    padding: 20px 10px;
 
     &__inner {
-      text-align: center;
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      gap: 30px;
+
+      @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+      }
+
+      .image {
+        clip-path: ellipse(43% 45% at 50% 49%);
+      }
+
+      .introduction {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        gap: 20px;
+
+        .greeting {
+          font-size: 48px;
+        }
+
+        .job {
+          font-size: 21px;
+        }
+
+        .bio {
+          word-spacing: 5px;
+        }
+
+      }
     }
   }
 
